377bdc8ec2075bbfc37eacf1b6302d884c6d9e2b
kipkoechke <chepkwonyke1@gmail.com>
2023-07-19 07:25:57 -0700

    Added function prototype to the header file

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*-------------Libraries------------*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+/*----------Funtion Prototypes for atoi.c-----*/
+int interactive_mode(info_t *info);
+#endif
