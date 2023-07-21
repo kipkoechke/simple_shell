8c1b943b3d76cd49562e02cd717c67cbac032eb9
kipkoechke <chepkwonyke1@gmail.com>
2023-07-21 05:16:10 -0700

    Worked on getting the value of an environment variable

--- environment.c
+++ environment.c
@@ -15,3 +15,34 @@ int my_env(info_t *info)
 
 	return (0);
 }
+
+/**
+ * _getenv - Gets the value of an environment variable.
+ * @info: Structure containing potential arguments.
+ * 			Used to maintain constant function prototype.
+ * @name: Env var name.
+ *
+ * Return: The value of the environment variable, or NULL if not found.
+ */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	/* Loop through the environment linked list */
+	while (node)
+	{
+		/* Check if the current node's string starts with the given name */
+		p = starts_with(node->str, name);
+
+		/** If a match is found,
+		 *	return the address of the next character (value) */
+		if (p && *p)
+			return (p);
+
+		node = node->next;
+	}
+
+	/* Return NULL if the environment variable is not found */
+	return (NULL);
+}
